807e735fd7cc7c76f076cd2a40c8299ecbae721e
ayuda002 <yudai02@puc.pu-toyama.ac.jp>
2022-05-31 10:19:21 +0900

    hoge

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '富山県立大学',
+      title: '富山県立大学(非公式)',
       theme: ThemeData(
           // This is the theme of your application.
           //
